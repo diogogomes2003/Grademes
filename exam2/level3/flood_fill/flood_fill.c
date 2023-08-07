0330a23b156d3f1fd6c11f263227b8fa232f0b5e
Diogo Gomes <120406673+diogogomes2003@users.noreply.github.com>
2023-08-07 10:02:57 +0100

    Update flood_fill.c

--- exam2/level3/flood_fill/flood_fill.c
+++ exam2/level3/flood_fill/flood_fill.c
@@ -10,11 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct  s_point
+typedef struct s_point
 {
-    int           x;
-	int           y;
-}               t_point;
+	int	x;
+	int	y;
+}	t_point;
 
 void	fill(char **tab, t_point size, int x, int y, char to_fill)
 {
@@ -27,4 +27,9 @@ void	fill(char **tab, t_point size, int x, int y, char to_fill)
 	fill(tab, size, x + 1, y, to_fill);
 	fill(tab, size, x, y - 1, to_fill);
 	fill(tab, size, x, y + 1, to_fill);
+}
+
+void	flood_fill(char **tab, t_point size, t_point begin)
+{
+	fill(tab, size, begin.x, begin.y, tab[begin.y][begin.x]);
 }
